9ec4d0ae0fdfb7ca10263fba8db926f52ab182c3
dnagendr <dnagendr@akamai.com>
2017-12-12 17:06:09 +0530

    My first Java application

---
+++ src/com/srccodes/example/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.srccodes.example;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//My first line of code in Java :-)
+		System.out.println("Hello World - Welcome to JAVA - Darshan Kale");
+	}
+}
